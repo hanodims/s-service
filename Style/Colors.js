80c29db0444f82987addab76622d04b627fce2be
hanodims <hend--mohammed@outlook.com>
2021-10-28 11:42:35 +0300

    first

---
+++ Style/Colors.js
@@ -0,0 +1,21 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+const darkBlue = "#1C396A";
+const blue = "#2B87C8";
+const green = "#20B3B6";
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+  },
+};
