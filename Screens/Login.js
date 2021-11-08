16716996dfc8660bf2fc8a2c54c5d2833324c0d2
hanodims <hend--mohammed@outlook.com>
2021-11-08 11:21:00 +0300

    login

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState("email@tst.com");
+  const [password, setPassword] = useState("h");
   return (
     <View style={styles.container}>
       <TextInput
@@ -14,6 +21,18 @@ export default function Login({ navigation }) {
         textAlign={"left"}
         style={{ borderWidth: 1, borderRadius: 30, height: 50, width: "100%" }}
       />
+      <TextInput
+        autoComplete={"password"}
+        defaultValue={password}
+        maxLength={150}
+        onChangeText={setPassword}
+        textAlign={"left"}
+        style={{ borderWidth: 1, borderRadius: 30, height: 50, width: "100%" }}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
